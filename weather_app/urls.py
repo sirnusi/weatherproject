a7cb3e6cd1f1b8a9bae5446cbd0129f5bd518fed
sirnusi <davidsanusi360@gmail.com>
2021-12-13 11:09:20 +0100

    weather commit

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path('', views.NoteList.as_view(), name='note-list'),
     path('<int:pk>/', views.NoteDetail.as_view(), name='note-detail'),
     path('categories/', views.CategoryList.as_view(), name='category-list'),
+    path('categories/create/', views.CategoryCreate.as_view(), name='category-create'),
     path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
     
 ]
--- weather_app/views.py
+++ weather_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView, CreateAPIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .models import Note, Category
 from .serializers import NoteSerializer, CategorySerializer
 from .permissions import NotePermission
@@ -7,25 +8,28 @@ from .permissions import NotePermission
 class CategoryList(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_class = []
+    permission_classes = [IsAuthenticated]
 
 class CategoryCreate(CreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_class = []
+    permission_classes = [IsAdminUser]
 
 class CategoryDetail(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    
+    permission_classes = [IsAdminUser]
     
 class NoteList(ListCreateAPIView):
-    queryset = Note.objects.all()
     serializer_class = NoteSerializer
-    permission_class = [NotePermission]
+    permission_classes = [NotePermission]
     
+    def get_queryset(self):
+        myown = Note.objects.filter(owner=self.request.user)
+        return myown                
+        
     
 class NoteDetail(RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
-    permission_class = [NotePermission]
+    permission_classes = [NotePermission]
