5d31643b9ab629662adfd8d01c4da5a6a9921de0
sirnusi <davidsanusi360@gmail.com>
2021-12-13 14:53:46 +0100

    weather commit

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView, CreateAPIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Note, Category
 from .serializers import NoteSerializer, CategorySerializer
 from .permissions import NotePermission
@@ -21,9 +21,10 @@ class CategoryDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAdminUser]
     
 class NoteList(ListAPIView):
+    # queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [NotePermission]
-    
+
     def get_queryset(self):
         my_notes = Note.objects.filter(owner=self.request.user)
         return my_notes                
