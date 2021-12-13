97556675088346242dcda81aa11e57a8363ea698
sirnusi <davidsanusi360@gmail.com>
2021-12-13 11:27:11 +0100

    weather commit

--- weather_app/views.py
+++ weather_app/views.py
@@ -20,14 +20,19 @@ class CategoryDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
     permission_classes = [IsAdminUser]
     
-class NoteList(ListCreateAPIView):
+class NoteList(ListAPIView):
     serializer_class = NoteSerializer
     permission_classes = [NotePermission]
     
     def get_queryset(self):
-        myown = Note.objects.filter(owner=self.request.user)
-        return myown                
-        
+        my_notes = Note.objects.filter(owner=self.request.user)
+        return my_notes                
+
+class NoteCreate(CreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [NotePermission]
+    
     
 class NoteDetail(RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
