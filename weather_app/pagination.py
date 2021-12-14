166ea017032123b673a4cb8ef361d4aba0cd7d10
sirnusi <davidsanusi360@gmail.com>
2021-12-14 16:31:23 +0100

    weather commit

--- weather_app/pagination.py
+++ weather_app/pagination.py
@@ -4,3 +4,5 @@ class NotePagination(PageNumberPagination):
     page_size = 3
     # page_query_param = 'notes'
     page_size_query_param = 'notes'
+    max_page_size = 5
+    last_page_strings = 'end'
