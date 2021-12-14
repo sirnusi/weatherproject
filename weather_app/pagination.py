89acc2ca498b4483fcd2d8b19808f5295a381f10
sirnusi <davidsanusi360@gmail.com>
2021-12-14 16:22:18 +0100

    weather commit

--- weather_app/pagination.py
+++ weather_app/pagination.py
@@ -3,3 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 class NotePagination(PageNumberPagination):
     page_size = 3
     # page_query_param = 'notes'
+    page_size_query_param = 'notes'
