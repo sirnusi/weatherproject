69c1b417312a943e1c42dbecfe106fa17062cc81
sirnusi <davidsanusi360@gmail.com>
2021-12-23 14:45:36 +0100

    weather commit

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', views.registration_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
