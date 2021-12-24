fcae6f220b561d1a8bbbcc5d6d88ee719a26fa15
sirnusi <davidsanusi360@gmail.com>
2021-12-24 12:08:40 +0100

    weather commit

--- user_app/tests.py
+++ user_app/tests.py
@@ -13,4 +13,23 @@ class RegistrationTestCase(APITestCase):
             'password2': 'Password123'
         }
         response = self.client.post(reverse('register'), data)
-        self.assertEqual(response.status_code, status.HTTP_200_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+class LoginLogoutTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='testcase', password='Password123')
+        
+    def test_login(self):
+        data = {
+            'username':'testcase',
+            'password': 'Password123'
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_logout(self):
+        self.token = Token.objects.get(user__username='testcase')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- weather_app/tests.py
+++ weather_app/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.http import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+from .models import Category, Note
+class CategoryTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', password='Password@123')
+        self.token = Token.objects.get(user__username='example')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.category = Category.objects.create(name='New Sports', website='https://newsports.com')
+    
+    def test_category_create(self):
+        data = {
+            'name': 'Sports',
+            'website': 'https://sports.com'
+        }
+        response = self.client.post(reverse('category-create'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_category_list(self):
+        response = self.client.get(reverse('category-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_category_details(self):
+        response = self.client.get(reverse('category-detail', args=(self.category.id, )))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_category_update(self):
+        response = self.client.put(reverse('category-detail', args=(self.category.id, )))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_category_delete(self):
+        response = self.client.delete(reverse('category-detail', args=(self.category.id, )))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+
+class NoteTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='example', password='NewPassword123')
+        self.token = Token.objects.get(user__username='example')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        self.category = Category.objects.create(name='Sports', website='https://sports.com')
+        self.note = Note.objects.create(title='New Note', owner=self.user, 
+                                        category=self.category, text='Wonderful Boy')
+    
+    def test_note_create(self):
+        data = {
+            'title':'example',
+            'owner': self.user,
+            'category': 'Sports',
+            'text': 'Wonderful Boy' 
+        }
+        response = self.client.post(reverse('note-create'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
