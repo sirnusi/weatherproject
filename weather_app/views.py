48285cda37fe3b0a3fbebba6a405a03771dc8cca
sirnusi <davidsanusi360@gmail.com>
2021-12-25 17:42:44 +0100

    weather commit

--- weather_app/views.py
+++ weather_app/views.py
@@ -2,6 +2,7 @@ from django.urls.base import reverse
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework import filters
+from rest_framework import serializers
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Note, Category
 from .serializers import NoteSerializer, CategorySerializer
@@ -26,17 +27,19 @@ class CategoryDetail(RetrieveUpdateDestroyAPIView):
     
     
 class NoteList(ListAPIView):
-    queryset = Note.objects.all()
+    # queryset = Note.objects.all()
     serializer_class = NoteSerializer
     filter_backends = [DjangoFilterBackend] #filter by the fields in our models.py in your url
     filterset_fields = ['category__name', 'owner__username']#the exact same word as it is on the database.
     pagination_class = NotePagination
 
     #User Anonymous
-    # def get_queryset(self):
-    #     owner = self.request.user
-    #     my_notes = Note.objects.filter(owner)
-    #     if owner                
+    def get_queryset(self):
+        if self.request.user.is_authenticated == False:
+            raise serializers.ValidationError({'error':'Try to login to access your saved notes'})
+        
+        my_notes = Note.objects.filter(owner=self.request.user)
+        return my_notes             
 
 class NoteSearch(ListAPIView):
     queryset = Note.objects.all()
