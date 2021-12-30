0bfaaf2b0f5b2f84d127a47b847a8d32090caa12
sirnusi <davidsanusi360@gmail.com>
2021-12-30 09:40:51 +0100

    weather commit

--- user_app/views.py
+++ user_app/views.py
@@ -1,9 +1,10 @@
-from . import models
+# from . import models
 from .serializers import RegistrationSerializer
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['POST',])
@@ -26,8 +27,12 @@ def registration_view(request):
             data['username'] = account.username
             data['email'] = account.email
             
-            token = Token.objects.get(user=account).key
-            data['token'] = token
+            # token = Token.objects.get(user=account).key
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
         else:
             data = serializer.errors
             
--- weather_project/settings.py
+++ weather_project/settings.py
@@ -133,6 +133,6 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
